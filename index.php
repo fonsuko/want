1de6e2f1e1b2f114789f4e8815f25cf775f553b0
fonsuko <fonsukoth@gmail.com>
2017-07-24 10:11:11 +0700

    edit exchange record

--- index.php
+++ index.php
@@ -181,74 +181,58 @@
 					<h2>บันทึกข้อมูลโครงสร้างบ่อพัก</h2>
 					<i class="fa fa-2x fa-angle-down"></i>
 
-					</div>
-					</div>
-				</div>
-			</div>
-			</div>
-		</div>
-		<div class="container">
-		<div class="row">
-			<div class="col-lg-2 col-lg-offset-5">
-				<hr class="marginbot-50">
-			</div>
-		</div>
-        <div class="row">
-            <div class="col-sm-3 col-md-3">
-				<div class="wow fadeInLeft" data-wow-delay="0.2s">
-                <div class="service-box">
-					<div class="service-icon">
-						<img src="img/icons/service-icon-1.png" alt="" />
-					</div>
-					<div class="service-desc">
-						<h5>Print</h5>
-						<p>Vestibulum tincidunt enim in pharetra malesuada. Duis semper magna metus electram accommodare.</p>
-					</div>
-                </div>
-				</div>
-            </div>
-			<div class="col-sm-3 col-md-3">
-				<div class="wow fadeInUp" data-wow-delay="0.2s">
-                <div class="service-box">
-					<div class="service-icon">
-						<img src="img/icons/service-icon-2.png" alt="" />
-					</div>
-					<div class="service-desc">
-						<h5>Web Design</h5>
-						<p>Vestibulum tincidunt enim in pharetra malesuada. Duis semper magna metus electram accommodare.</p>
-					</div>
-                </div>
-				</div>
-            </div>
-			<div class="col-sm-3 col-md-3">
-				<div class="wow fadeInUp" data-wow-delay="0.2s">
-                <div class="service-box">
-					<div class="service-icon">
-						<img src="img/icons/service-icon-3.png" alt="" />
-					</div>
-					<div class="service-desc">
-						<h5>Photography</h5>
-						<p>Vestibulum tincidunt enim in pharetra malesuada. Duis semper magna metus electram accommodare.</p>
-					</div>
-                </div>
-				</div>
-            </div>
-			<div class="col-sm-3 col-md-3">
-				<div class="wow fadeInRight" data-wow-delay="0.2s">
-                <div class="service-box">
-					<div class="service-icon">
-						<img src="img/icons/service-icon-4.png" alt="" />
-					</div>
-					<div class="service-desc">
-						<h5>Cloud System</h5>
-						<p>Vestibulum tincidunt enim in pharetra malesuada. Duis semper magna metus electram accommodare.</p>
-					</div>
-                </div>
-				</div>
-            </div>
-        </div>
-		</div>
-	</section>
+          <div class="row">
+              <div class="col-lg-8">
+                  <div class="boxed-grey">
+                      <form id="contact-form">
+                      <div class="row">
+                          <div class="col-md-6">
+                              <div class="form-group">
+                                  <label for="name">
+                                      Name</label>
+                                  <input type="text" class="form-control" id="name" placeholder="Enter name" required="required" />
+                              </div>
+                              <div class="form-group">
+                                  <label for="email">
+                                      Email Address</label>
+                                  <div class="input-group">
+                                      <span class="input-group-addon"><span class="glyphicon glyphicon-envelope"></span>
+                                      </span>
+                                      <input type="email" class="form-control" id="email" placeholder="Enter email" required="required" /></div>
+                              </div>
+                              <div class="form-group">
+                                  <label for="subject">
+                                      Subject</label>
+                                  <select id="subject" name="subject" class="form-control" required="required">
+                                      <option value="na" selected="">Choose One:</option>
+                                      <option value="service">General Customer Service</option>
+                                      <option value="suggestions">Suggestions</option>
+                                      <option value="product">Product Support</option>
+                                  </select>
+                              </div>
+                          </div>
+
+                          <div class="col-md-6">
+                              <div class="form-group">
+                                  <label for="name">
+                                      Message</label>
+                                  <textarea name="message" id="message" class="form-control" rows="9" cols="25" required="required"
+                                      placeholder="Message"></textarea>
+                              </div>
+                          </div>
+                          <div class="col-md-12">
+                              <button type="submit" class="btn btn-skin pull-right" id="btnContactUs">
+                                  Send Message</button>
+                          </div>
+                      </div>
+                      </form>
+                  </div>
+              </div>
+
+
+
+
+
 	<!-- /Section: services -->
 
 
